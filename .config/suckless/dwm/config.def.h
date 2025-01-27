f06aeb9fe1af9a44289734c2639ebfa96812af4c
bazeeel <deakfefe77@gmail.com>
2025-01-27 10:39:03 +0100

    update dwm

--- .config/suckless/dwm/config.def.h
+++ .config/suckless/dwm/config.def.h
@@ -1,16 +1,16 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx       = 6;        /* border pixel of windows */
+static const unsigned int borderpx       = 3;        /* border pixel of windows */
 static const unsigned int gappx          = 20;        /* gaps between windows */
 static const unsigned int snap           = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const unsigned int systrayspacing = 3;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray             = 1;     /* 0 means no systray */
 static const int showbar                 = 1;        /* 0 means no bar */
 static const int topbar                  = 1;        /* 0 means bottom bar */
-static const char *fonts[]               = { "JetBrains Mono Nerd:size=10" , "JoyPixels:size=8" , "FontAwesome:size=11:antialias=true:autohint=true" };
+static const char *fonts[]               = { "JetBrains Mono Nerd:size=9" , "JoyPixels:size=8" , "FontAwesome:size=10:antialias=true:autohint=true" };
 static const char dmenufont[]            = "JetBrainsMono Nerd:size=11";
 static const char col_1[]                = "#282a36";  /* background color of bar and border color unfocused windows */
 static const char col_2[]                = "#f8f8f2";  /* foreground color of bar and tags */
@@ -38,9 +38,13 @@ static const Rule rules[] = {
 	{ "st-256color",    NULL,       NULL,       0,             1,           -1 },
 	{ "Microsoft-edge", NULL,       NULL,       1 << 0,        0,           -1 },
 	{ "discord",        NULL,       NULL,       1 << 1,        0,          -1 },
-	{ "subl3",           NULL,       NULL,       1 << 2,        0,          -1 },
+	{ "Thunar",         NULL,       NULL,       1 << 7,        0,          -1 },
 	{ "steam",          NULL,       NULL,       1 << 4,        0,          -1 },
     { "Pavucontrol",    NULL,       NULL,       0,             1,          -1 },
+    { "upc.exe",        NULL,       NULL,       0,             1,          -1 },
+    { "Sublime_text",    NULL,       NULL,      1 << 3,        0,          -1 },
+    { "Lxappearance",        NULL,       NULL,       0,             1,          -1 },
+
 };
 
 /* layout(s) */
@@ -79,8 +83,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run -c -g 1 -l 10 ", "-m", dmenumon, "-fn", dmenufont, NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "160x40", "-e", "ncmpcpp", NULL };
-static const char *termcmd[] = { "st", "-g", "160x40", NULL };
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "110x30", "-e", "ncmpcpp", NULL };
+static const char *termcmd[] = { "st", "-g", "110x30", NULL };
 
 #include "movestack.c"
 static Key keys[] = {
